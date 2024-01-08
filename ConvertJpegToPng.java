6d3b723689f7b69425034384316c7f8a36bc2387
Timothy Kimball <82687508+TimothyKimball-design@users.noreply.github.com>
2024-01-07 21:03:17 -0500

    Update JpegToPngConverter.java

--- ConvertJpegToPng.java
+++ ConvertJpegToPng.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.convert;
 
@@ -9,7 +6,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class ConvertJpegToPng {
+public class JpegToPngConverter {
 
     public static void main(String[] args) {
         // The path to the input JPEG image.
